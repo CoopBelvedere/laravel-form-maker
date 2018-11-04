07f927eadc4bfe35a5647dbe9b50655ad49fc6de
Pascal <pascalboocher@gmail.com>
2018-11-04 21:55:15 +0000

    Fixed migration table class name deprecated

--- database/migrations/create_form_maker_tables.php
+++ database/migrations/create_form_maker_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFormBuilderTables extends Migration
+class CreateFormMakerTables extends Migration
 {
     public function up()
     {
