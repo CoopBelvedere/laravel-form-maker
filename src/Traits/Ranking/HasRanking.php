ece856b606d32dddadf3d8a8ed58a446e1ee263b
pascalboucher <pascalboocher@gmail.com>
2019-05-28 19:16:38 +0000

    refactored the way we handle the creation of rankings to limit the number of unused rows in the db

--- src/Traits/Ranking/HasRanking.php
+++ src/Traits/Ranking/HasRanking.php
@@ -3,9 +3,8 @@
 namespace Belvedere\FormMaker\Traits;
 
 use Belvedere\FormMaker\Contracts\Ranking\RankerContract;
-use Belvedere\FormMaker\Listeners\CreateRanking;
 use Belvedere\FormMaker\Listeners\DeleteRanking;
-use Illuminate\Database\Eloquent\Model;
+use Belvedere\FormMaker\Models\Model;
 
 trait HasRanking
 {
@@ -21,10 +20,6 @@ trait HasRanking
      */
     protected static function bootHasRanking()
     {
-        static::created(function (Model $model) {
-            event(new CreateRanking($model));
-        });
-
         static::retrieved(function (Model $model) {
             $model->load('ranking');
         });
@@ -32,6 +27,35 @@ trait HasRanking
         static::deleted(function (Model $model) {
             event(new DeleteRanking($model));
         });
+    }
+
+    /**
+     * Add a node in the ranking
+     *
+     * @param \Belvedere\FormMaker\Models\Model $node
+     * @return void
+     */
+    protected function addInRanking(Model $node): void
+    {
+        if (is_null($this->ranking)) {
+            $this->createRanking();
+        }
+
+        $this->ranking->add($node);
+    }
+
+    /**
+     * Create a ranking
+     *
+     * @return void
+     */
+    protected function createRanking(): void
+    {
+        $ranking = resolve(RankerContract::class);
+
+        $ranking->ranks = [];
+
+        $this->ranking()->save($ranking);
     }
 
     /**
