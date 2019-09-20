a329bc157d59cec25392a5bdb7c4865f0af68e94
pascalboucher <pascalboocher@gmail.com>
2019-09-20 19:19:10 +0000

    Added addOption method

--- src/Traits/Nodes/HasOptions.php
+++ src/Traits/Nodes/HasOptions.php
@@ -2,7 +2,41 @@
 
 namespace Belvedere\FormMaker\Traits\Nodes;
 
+use Belvedere\FormMaker\Contracts\{
+    Inputs\Option\OptionerContract,
+    Repositories\NodeRepositoryContract
+};
+
 trait HasOptions
 {
-    // TODO
+    /**
+     * Add a node to the parent model.
+     *
+     * @param array $attributes
+     * @return \Belvedere\FormMaker\Contracts\Inputs\Option\OptionerContract $option
+     */
+    public function addOption(array $attributes): OptionerContract
+    {
+        $nodeRepository = resolve(NodeRepositoryContract::class);
+
+        $option = $nodeRepository->create($this, 'option')
+            ->withHtmlAttributes($attributes)
+            ->saveAndFirst();
+
+        $this->addInRanking($option);
+
+        return $option;
+    }
+
+    /**
+     * Add options for the input.
+     *
+     * @param array ...$options
+     * @return self
+     * @throws \Exception
+     */
+    public function addOptions(array ...$options): self
+    {
+        dd($options);
+    }
 }
