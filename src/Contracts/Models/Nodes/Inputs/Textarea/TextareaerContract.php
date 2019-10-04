bd54638e098090eb1b5c7e976982637feb1d6344
Pascal Boucher <pascalboocher@gmail.com>
2019-10-04 15:36:44 -0400

    Apply fixes from StyleCI (#13)

--- src/Contracts/Models/Nodes/Inputs/Textarea/TextareaerContract.php
+++ src/Contracts/Models/Nodes/Inputs/Textarea/TextareaerContract.php
@@ -2,8 +2,8 @@
 
 namespace Belvedere\FormMaker\Contracts\Models\Nodes\Inputs\Textarea;
 
-use Belvedere\FormMaker\Contracts\Models\Nodes\Inputs\InputContract;
 use Belvedere\FormMaker\Contracts\Traits\Text\HasTextContract;
+use Belvedere\FormMaker\Contracts\Models\Nodes\Inputs\InputContract;
 
 interface TextareaerContract extends HasTextContract, InputContract
 {
